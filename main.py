ec873edb0823227798eab3f82fd23517f0565ead
DonMins <maks000156@yandex.ru>
2019-02-24 01:30:02 +0400

    коменты

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import application
 
 def main(args):
-    app = application.Application(args)
+    app = application.Application(args)# создали объект класса- переход в Init
     app.execute()
 
 if __name__ == "__main__":
